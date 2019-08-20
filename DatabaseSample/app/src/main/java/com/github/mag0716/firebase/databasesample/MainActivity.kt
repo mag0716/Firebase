a230336caef14622c924b3c46b1b9d1685a5cab2
Masashi Kishimoto <mkishimoto0716@gmail.com>
2019-08-20 22:55:10 +0900

    データ追加処理実装

--- DatabaseSample/app/src/main/java/com/github/mag0716/firebase/databasesample/MainActivity.kt
+++ DatabaseSample/app/src/main/java/com/github/mag0716/firebase/databasesample/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.github.mag0716.firebase.databasesample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "DatabaseSample"
+    }
+
+    private lateinit var database: FirebaseFirestore
+    private lateinit var addButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        database = FirebaseFirestore.getInstance()
+
+        addButton = findViewById(R.id.add_button)
+        addButton.setOnClickListener { addData() }
+    }
+
+    private fun addData() {
+        val data = hashMapOf(
+            "datetime" to System.currentTimeMillis(),
+            "data" to "data"
+        )
+
+        database.collection("data")
+            .add(data)
+            .addOnSuccessListener { documentReference ->
+                Log.d(
+                    TAG,
+                    "addData success: ${documentReference.id}"
+                )
+            }
+            .addOnFailureListener { e -> Log.w(TAG, "addData failure", e) }
     }
 }
